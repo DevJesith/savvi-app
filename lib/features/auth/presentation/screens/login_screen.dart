b624d6c8c3de08a21691af937a73e14de9d9727a
Jesith Mejia <jesithmanuel@gmail.com>
2026-06-25 22:44:25 -0500

    chore(ui): reorganize presentation folder structure
    - Add print basic in Buttons

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:savvi/core/providers/auth_provider.dart';
+import 'package:savvi/features/auth/presentation/providers/login_providers.dart';
 import 'package:savvi/shared/widgets/inputs_reutilizable_widgets.dart';
 
 class LoginScreen extends ConsumerWidget {
@@ -10,50 +11,74 @@ class LoginScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // MediaQuery para obtener dimensiones del dispositivo (Responsive), es decir cuantos pixeles tiene esta pantalla de ancho y largo
     final size = MediaQuery.of(context).size;
-    final formKey = ref.watch(formKeyProvider);
+
+    // Obtenemos los estados desde el archivo de providers
+    final formKey = ref.watch(loginFormKeyProvider);
     final emailController = ref.watch(emailControllerProvider);
     final passwordController = ref.watch(passwordControllerProvider);
-    final obscureTextt = ref.watch(obscureTextProvider);
+    final isObscure = ref.watch(obscureTextProvider);
 
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: const Color(0xFFF8F6F5),
-        body: Center(
+    return Scaffold(
+      backgroundColor: const Color(0xFFF8F6F5),
+      // Evita que el contenido toque el notch/camara
+      body: SafeArea(
+        child: Center(
+          // El mejor amigo del responsive. Cuando el usuario abre el teclado para escribir, el espacio de la pantalla se reduce a la mitad. Este widget permite que la app se deslice y no "explote" al abrir el teclado.
           child: SingleChildScrollView(
             child: Container(
-              width: size.width * 0.9,
-              padding: EdgeInsets.all(20),
+              width:
+                  size.width * 0.9, // Responsive 90% del ancho del dispositivo
+              padding: const EdgeInsets.all(24),
               decoration: BoxDecoration(
                 color: Colors.white,
-                borderRadius: BorderRadius.circular(16),
+                borderRadius: BorderRadius.circular(24),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.05),
+                    blurRadius: 10,
+                    offset: const Offset(0, 5),
+                  ),
+                ],
+                border: BoxBorder.all(
+                  color: Color.fromARGB(34, 255, 73, 41),
+                  width: 2,
+                ),
               ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                // mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+                  // SECCION DE ENCABEZADO: STACK CENTRADO ABSOLUTO
                   Stack(
+                    // Asegura que todos los hijos que NO tengan un Align
+                    // propio se ubiquen en el centro del Stack
                     alignment: Alignment.center,
                     children: [
+                      // Usamos Align para anclar el boton a la izquierda
+                      // sin que interfiera con la posicion del titulo
                       Align(
                         alignment: Alignment.centerLeft,
                         child: IconButton(
                           onPressed: () {
+                            //TODO: Boton funcional pronto cuando este completo el Onboarding
                             print('Atrás');
                           },
                           icon: Icon(Icons.arrow_back),
                           style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all(
+                            backgroundColor: WidgetStateProperty.all(
                               Color(0x1AFF4929),
                             ),
-                            foregroundColor: MaterialStateProperty.all(
+                            foregroundColor: WidgetStateProperty.all(
                               Color(0xFFFF4929),
                             ),
-                            shape: MaterialStateProperty.all(CircleBorder()),
+                            shape: WidgetStateProperty.all(CircleBorder()),
                           ),
                         ),
                       ),
 
+                      // Como el Stack tiene aligmente.center,
+                      // este texto se queda perfectamente centrado respecto al Container padre.
                       Text(
                         'Bienvenido',
                         style: TextStyle(
@@ -65,40 +90,49 @@ class LoginScreen extends ConsumerWidget {
                   ),
 
                   SizedBox(height: 10),
+
+                  // Titulo de iniciar sesion
                   Text(
                     'Iniciar sesion',
                     style: GoogleFonts.plusJakartaSans(
                       fontSize: 32,
                       fontWeight: FontWeight.bold,
                     ),
-                    // textAlign: TextAlign.left,
                   ),
 
                   const SizedBox(height: 15),
 
+                  // Subtitulo de proposito
                   Text(
                     'Gestiona tus finanzas de forma segura y alcanza tus metas',
                     style: GoogleFonts.plusJakartaSans(fontSize: 16),
                   ),
+
                   const SizedBox(height: 16),
 
+                  // FORMULARIO
                   Form(
+                    // Asignamos la llave (el control remoto)
+                    // Ahora ref.watch(LoginFormKeyProvider) tiene el poder sobre este form
                     key: formKey,
                     child: Column(
                       children: [
-                        // Email
+                        // Email - Widget reutilizable/modificable
                         InputsReutilizableWidgets(
                           controller: emailController,
                           nameInput: 'Correo electronico',
+                          //Validacion del campo
                           validator: (value) {
+                            // Si se encuentra vacio retorna mensaje
                             if (value == null || value.trim().isEmpty) {
                               return 'Ingresa tu correo';
                             }
+                            // Parametro que cumpla la direccion del email
                             final emailRegex = RegExp(r'^[^@]+@[^@]+\.[^@]+');
                             if (!emailRegex.hasMatch(value.trim())) {
                               return 'Correo inválido';
                             }
-                            return null;
+                            return null; // Significa que "Todo esta perfecto"
                           },
                           decoration: InputDecoration(
                             hintText: 'ejemplo@correo.com',
@@ -122,19 +156,16 @@ class LoginScreen extends ConsumerWidget {
 
                         const SizedBox(height: 15),
 
-                        // Password
+                        // Password - Widget reutilizable/modificable
                         InputsReutilizableWidgets(
                           controller: passwordController,
-                          obscuredText: obscureTextt,
+                          obscuredText: isObscure,
                           keyboardType: TextInputType.visiblePassword,
                           nameInput: 'Contraseña',
                           validator: (value) {
+                            // Si se encuentra vacio retorna mensaje
                             if (value == null || value.trim().isEmpty) {
-                              return 'Ingresa tu correo';
-                            }
-                            final emailRegex = RegExp(r'^[^@]+@[^@]+\.[^@]+');
-                            if (!emailRegex.hasMatch(value.trim())) {
-                              return 'Correo inválido';
+                              return 'Ingresa tu contraseña';
                             }
                             return null;
                           },
@@ -155,9 +186,10 @@ class LoginScreen extends ConsumerWidget {
                               ),
                               borderRadius: BorderRadius.circular(20),
                             ),
+                            // Funcionalidad del ocultar/mostrar contraseña
                             suffixIcon: IconButton(
                               icon: Icon(
-                                obscureTextt
+                                isObscure
                                     ? Icons.visibility_off
                                     : Icons.visibility,
                               ),
@@ -171,7 +203,10 @@ class LoginScreen extends ConsumerWidget {
                     ),
                   ),
 
+                  // Posicionamiento LATERAL
                   Align(
+                    // Usamos Align porque el Column padre tiene crossAxisAligment.start.
+                    // Si no psuieramos este Align, el boton apareciera a la izquierda
                     alignment: Alignment.centerRight,
                     child: TextButton(
                       onPressed: () {
@@ -191,6 +226,7 @@ class LoginScreen extends ConsumerWidget {
 
                   const SizedBox(height: 5),
 
+                  // Boton de iniciar sesion
                   SizedBox(
                     width: double.infinity,
                     height: 50,
@@ -202,7 +238,18 @@ class LoginScreen extends ConsumerWidget {
                           borderRadius: BorderRadiusGeometry.circular(50),
                         ),
                       ),
-                      onPressed: () {},
+                      onPressed: () {
+                        // DISPARADORES DE VALIDACION
+
+                        // Usamos la llave para preguntar: "¿Todos los validadores devolvieron null?"
+                        if (formKey.currentState!.validate()) {
+                          //SI: los datos son correctos. Aqui llamariamos al Repositorio de iniciar sesion.
+                          print("Formulario valido. Iniciando sesion...");
+                        } else {
+                          // NO: Flutter automaticamente pondre los textos en rojo.
+                          print("ERROR: hay campos invalidos");
+                        }
+                      },
                       child: Text(
                         'Ingresar',
                         style: GoogleFonts.plusJakartaSans(
@@ -215,12 +262,13 @@ class LoginScreen extends ConsumerWidget {
 
                   SizedBox(height: 20),
 
+                  // Divisor para metodos de iniciar de sesion rapido
                   Row(
                     children: [
                       Expanded(
                         child: Divider(
                           thickness: 1, // grosor
-                          color: Colors.grey[400], // color
+                          color: Colors.grey[400],
                         ),
                       ),
                       Padding(
@@ -245,6 +293,7 @@ class LoginScreen extends ConsumerWidget {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
+                      // Boton de iniciar sesion con Google
                       ElevatedButton(
                         onPressed: () async {
                           try {
@@ -286,8 +335,12 @@ class LoginScreen extends ConsumerWidget {
                         ),
                       ),
 
+                      // Boton de iniicar sesion con iOS (por el momento)
                       ElevatedButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          //TODO: Implement sing in with iOS
+                          print('iOS');
+                        },
                         style: ElevatedButton.styleFrom(
                           backgroundColor: Colors.white,
                           foregroundColor: Colors.black,
@@ -327,8 +380,12 @@ class LoginScreen extends ConsumerWidget {
                         style: GoogleFonts.plusJakartaSans(fontSize: 16),
                       ),
 
+                      // Boton de registro manual
                       TextButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          //TODO: Implement Form manual
+                          print('Registro');
+                        },
                         child: Text(
                           'Registrate',
                           style: GoogleFonts.plusJakartaSans(
