66e745d5c7571cdf9126242b61783391eefc4eff
Jesith Mejia <jesithmanuel@gmail.com>
2026-08-28 21:45:28 -0500

    feat(auth): add security info card and scroll support to location profiling screen
    - Wrap layout in SingleChildScrollView to prevent overflow issues.
    - Add security card ("Tus datos están seguros")

--- lib/features/auth/presentation/screens/user_profiling/location_screen.dart
+++ lib/features/auth/presentation/screens/user_profiling/location_screen.dart
@@ -16,173 +16,231 @@ class LocationScreen extends ConsumerWidget {
     return Scaffold(
       backgroundColor: const Color(0xFFF8F6F5),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              //-----
-              Text(
-                "Cuentanos sobre ti",
-                style: GoogleFonts.plusJakartaSans(
-                  fontSize: 32,
-                  fontWeight: FontWeight.w800,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //-----
+                Text(
+                  "Cuentanos sobre ti",
+                  style: GoogleFonts.plusJakartaSans(
+                    fontSize: 32,
+                    fontWeight: FontWeight.w800,
+                  ),
                 ),
-              ),
-
-              const SizedBox(height: 15),
-
-              Text(
-                "Necesitamos estos datyos para configurar tu experiencia financiera personalizada y tu moeda local",
-                style: GoogleFonts.plusJakartaSans(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
+          
+                const SizedBox(height: 15),
+          
+                Text(
+                  "Necesitamos estos datyos para configurar tu experiencia financiera personalizada y tu moeda local",
+                  style: GoogleFonts.plusJakartaSans(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
-              ),
-
-              const SizedBox(height: 30),
-
-              Text(
-                "Nacionalidad",
-                style: GoogleFonts.plusJakartaSans(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
+          
+                const SizedBox(height: 30),
+          
+                Text(
+                  "Nacionalidad",
+                  style: GoogleFonts.plusJakartaSans(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
-              ),
-
-              const SizedBox(height: 16),
-
-              // --- COMPONENTE PERSONALIZADO ---
-              GestureDetector(
-                onTap: () {
-                  showCountryPicker(
-                    context: context,
-                    onSelect: (Country country) =>
-                        notifier.updateCountry(country),
-                    showPhoneCode: false,
-                    countryListTheme: CountryListThemeData(
-                      borderRadius: BorderRadius.circular(24),
-                      inputDecoration: InputDecoration(
-                        hintText: 'Buscar pais...',
-                        prefixIcon: const Icon(Icons.search),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
+          
+                const SizedBox(height: 16),
+          
+                // --- COMPONENTE PERSONALIZADO ---
+                GestureDetector(
+                  onTap: () {
+                    showCountryPicker(
+                      context: context,
+                      onSelect: (Country country) =>
+                          notifier.updateCountry(country),
+                      showPhoneCode: false,
+                      countryListTheme: CountryListThemeData(
+                        borderRadius: BorderRadius.circular(24),
+                        inputDecoration: InputDecoration(
+                          hintText: 'Buscar pais...',
+                          prefixIcon: const Icon(Icons.search),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 18,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(50),
+                      border: Border.all(
+                        color: state.selectedCountry.isNotEmpty
+                            ? const Color(0xFFFF4929)
+                            : const Color(0xFFFFE4E0),
+                        width: 1.5,
+                      ),
+                    ),
+                    child: Row(
+                      // Mostar bandera
+                      children: [
+                        Text(
+                          state.countryFlag.isNotEmpty
+                              ? state.countryFlag
+                              : "🏳️",
+                          style: const TextStyle(fontSize: 12),
+                        ),
+          
+                        const SizedBox(width: 12),
+          
+                        Text(
+                          state.selectedCountry.isNotEmpty
+                              ? "${state.selectedCountry} (${state.selectedCurrency})"
+                              : "Selecciona tu pais",
+                          style: GoogleFonts.plusJakartaSans(
+                            fontSize: 16,
+                            color: state.selectedCountry.isNotEmpty
+                                ? const Color(0xFF0F172A)
+                                : const Color(0xFF64748B),
+                            fontWeight: state.selectedCountry.isNotEmpty
+                                ? FontWeight.w600
+                                : FontWeight.normal,
+                          ),
+                        ),
+          
+                        const Spacer(),
+                        const Icon(
+                          Icons.keyboard_arrow_down_rounded,
+                          color: Color(0xFF64748B),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+          
+                const SizedBox(height: 12),
+          
+                // Informativo
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.info_outline,
+                      size: 14,
+                      color: Color(0xFF64748B),
+                    ),
+          
+                    const SizedBox(width: 8),
+          
+                    Expanded(
+                      child: Text(
+                        "Esto definira automaticamente tu divisa principal",
+                        style: GoogleFonts.plusJakartaSans(
+                          fontSize: 12,
+                          fontWeight: FontWeight.normal,
+                          color: Color(0xFF64748B),
                         ),
                       ),
                     ),
-                  );
-                },
-                child: Container(
-                  width: double.infinity,
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 20,
-                    vertical: 18,
-                  ),
+                  ],
+                ),
+          
+                const SizedBox(height: 30),
+          
+                // --- INFO CARD SEGURIDAD ---
+                Container(
+                  padding: const EdgeInsets.all(20),
                   decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(50),
-                    border: Border.all(
-                      color: state.selectedCountry.isNotEmpty
-                          ? const Color(0xFFFF4929)
-                          : const Color(0xFFFFE4E0),
-                      width: 1.5,
-                    ),
+                    color: const Color.fromARGB(255, 255, 238, 234),
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(color: const Color(0xFFFFE4E0), width: 1),
                   ),
                   child: Row(
-                    // Mostar bandera
                     children: [
-                      Text(
-                        state.countryFlag.isNotEmpty
-                            ? state.countryFlag
-                            : "🏳️",
-                        style: const TextStyle(fontSize: 12),
-                      ),
-
-                      const SizedBox(width: 12),
-
-                      Text(
-                        state.selectedCountry.isNotEmpty
-                            ? "${state.selectedCountry} (${state.selectedCurrency})"
-                            : "Selecciona tu pais",
-                        style: GoogleFonts.plusJakartaSans(
-                          fontSize: 16,
-                          color: state.selectedCountry.isNotEmpty
-                              ? const Color(0xFF0F172A)
-                              : const Color(0xFF64748B),
-                          fontWeight: state.selectedCountry.isNotEmpty
-                              ? FontWeight.w600
-                              : FontWeight.normal,
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          shape: BoxShape.circle,
+                        ),
+                        child: const Icon(
+                          Icons.verified_user_outlined,
+                          color: Color.fromARGB(255, 252, 60, 27),
+                          size: 24,
                         ),
                       ),
-
-                      const Spacer(),
-                      const Icon(
-                        Icons.keyboard_arrow_down_rounded,
-                        color: Color(0xFF64748B),
+                      const SizedBox(width: 16),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Tus datos están seguros",
+                              style: GoogleFonts.plusJakartaSans(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14,
+                                color: const Color(0xFF18181B),
+                              ),
+                            ),
+                            const SizedBox(height: 4),
+                            Text(
+                              "Utilizamos encriptación de grado bancario para proteger tu información personal.",
+                              style: GoogleFonts.plusJakartaSans(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w500,
+                                color: const Color(0xFF71717A),
+                                height: 1.4,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ],
                   ),
                 ),
-              ),
+          
+                // const Spacer(),
 
-              const SizedBox(height: 12),
-
-              // Informativo
-              Row(
-                children: [
-                  const Icon(
-                    Icons.info_outline,
-                    size: 14,
-                    color: Color(0xFF64748B),
-                  ),
-
-                  const SizedBox(width: 8),
-
-                  Expanded(
-                    child: Text(
-                      "Esto definira automaticamente tu divisa principal",
-                      style: GoogleFonts.plusJakartaSans(
-                        fontSize: 12,
-                        fontWeight: FontWeight.normal,
-                        color: Color(0xFF64748B),
+                const SizedBox(height: 250,),
+          
+                // Boton siguiente
+                SizedBox(
+                  width: double.infinity,
+                  height: 56,
+                  child: ElevatedButton(
+                    onPressed: state.selectedCountry.isEmpty
+                        ? null
+                        : () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (_) => OccupationScreen(),
+                              ),
+                            );
+                          },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFFFF4929),
+                      foregroundColor: Colors.white,
+                      disabledBackgroundColor: Colors.grey[300],
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16),
                       ),
                     ),
-                  ),
-                ],
-              ),
-
-              const Spacer(),
-
-              // Boton siguiente
-              SizedBox(
-                width: double.infinity,
-                height: 56,
-                child: ElevatedButton(
-                  onPressed: state.selectedCountry.isEmpty
-                      ? null
-                      : () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (_) => OccupationScreen(),
-                            ),
-                          );
-                        },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFFFF4929),
-                    foregroundColor: Colors.white,
-                    disabledBackgroundColor: Colors.grey[300],
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16),
+                    child: const Text(
+                      "Siguiente",
+                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                     ),
                   ),
-                  child: const Text(
-                    "Siguiente",
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
